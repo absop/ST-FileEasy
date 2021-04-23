c2768e1dc1901c2deb5a68f0fac8c022baa34749
absop <zlang0@163.com>
2021-04-23 15:21:58 +0800

    Added `FileEasyEventListener`

--- loader.py
+++ loader.py
@@ -1,1 +1,15 @@
+import webbrowser
+import sublime_plugin
+
 from .plugins import *
+
+
+class FileEasyEventListener(sublime_plugin.EventListener):
+    def on_load(self, view):
+        file_path = view.file_name()
+        extensions = ['.pdf']
+        for ext in extensions:
+            if file_path and file_path.endswith(ext):
+                webbrowser.open_new_tab(file_path)
+                # view.close()
+                return
